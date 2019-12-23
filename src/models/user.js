103e62af1f29ba6e2afa0449b14937b3de5e11d8
metagenes <48299631+metagenes@users.noreply.github.com>
2019-12-23 18:42:40 +0700

    update

--- src/models/user.js
+++ src/models/user.js
@@ -32,7 +32,7 @@ module.exports = {
     getUserHistory: id => {
         return new Promise ((resolve, reject) => {
             conn.query(
-                `SELECT P.name, V.status FROM user U JOIN transaction T ON U.id_user = T.id_user JOIN voucher V on V.id_user=T.id_user JOIN partner P ON P.id_partner = V.id_partner where U.id_user`,id,
+                `SELECT P.name, V.status FROM user U JOIN transaction T ON U.id_user = T.id_user JOIN voucher V on V.id_user=T.id_user JOIN partner P ON P.id_partner = V.id_partner where U.id_user = ?`,id,
                 (err, response) => {
                     if (!err) {
                         resolve (response);
